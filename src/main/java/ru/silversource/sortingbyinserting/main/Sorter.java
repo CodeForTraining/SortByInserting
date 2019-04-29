bf1f99944c15a5c9499ee64f04a6bcf91c330479
Andrey Belikov <belycov@rambler.ru>
2019-04-29 09:38:50 +0600

    Several syntax edits have been made

--- src/main/java/ru/silversource/sortingbyinserting/main/Sorter.java
+++ src/main/java/ru/silversource/sortingbyinserting/main/Sorter.java
@@ -34,7 +34,6 @@ public class Sorter extends Thread {
 
     private void sort(){
         List<String> list = null;
-        File fileOut = new File(file.getParent()+ "\\" + prefix + file.getName());
         try(BufferedReader reader = new BufferedReader(new FileReader(file))) {
             list = reader.lines().collect(Collectors.toList());
 
@@ -48,12 +47,16 @@ public class Sorter extends Thread {
             LOGGER.debug("Array is string: {}", list);
 
         } catch (FileNotFoundException ex) {
-            LOGGER.debug("File not found: {}", file.getPath());
+            LOGGER.info("File not found: {}", file.getPath());
+            return;
         }catch (IOException e){
-            LOGGER.debug("Can't read file: {}", file.getPath());
+            LOGGER.info("Can't read file: {}", file.getPath());
+            return;
         } catch (SortException e) {
             LOGGER.info("File not sorting as digital: {}", file.getPath());
+            return;
         }
+        File fileOut = new File(file.getParent()+ "\\" + prefix + file.getName());
         try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileOut))){
             for(String line: list) {
                 bufferedWriter.write(line);
